12c7af720d13d51f6480399c14a5454a3607fb56
Jiří Otáhal <xotahal@gmail.com>
2016-10-27 14:11:11 +0200

    Export RippleFeedback

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as COLOR from './styles/colors';
 
 export { COLOR };
 export { default as ThemeProvider } from './styles/ThemeProvider.react';
+export { default as RippleFeedback } from './RippleFeedback';
 // components
 export { default as ActionButton } from './ActionButton';
 export { default as Avatar } from './Avatar';
