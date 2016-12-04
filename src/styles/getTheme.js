2f2bedfbeec680bfd97b2fe700a93375f985d2b8
Zino Hofmann <zino@hofmann.amsterdam>
2016-12-05 00:22:12 +0100

    Fixed a bug where the list item component uses the wrong then color refrence

--- src/styles/getTheme.js
+++ src/styles/getTheme.js
@@ -333,7 +333,7 @@ export default function getTheme(theme, ...more) {
             },
             secondaryText: {
                 lineHeight: 22,
-                color: palette.secondaryText,
+                color: palette.secondaryTextColor,
                 ...typography.body1,
             },
             rightElementContainer: {
