d7ab3ef2785107ba9d8c472e1fe6c60b8ed0c4a6
Zino Hofmann <zino@hofmann.amsterdam>
2017-01-28 15:08:15 +0100

    Add show and hide animations [Toolbar] (#80)
    * Added bottom navigation component with dacumentation, as dricribed in the material design guidelines
    
    * Fixed a bug where the list item component uses the wrong then color refrence
    
    * Fixed a bug where the list item component uses a static background instad of canvas collor
    
    * Added onPress ad required prop type
    
    * Fix bottom navigation component
    
    - moves BottomNavigationAction under BottomNavigation.Action
    - makes styles global, moves them to getTheme.js
    - adds ability to use action without label
    - adds missing paddingTop
    - adds missing comments
    - fixes lint
    
    * Added show and hide animations to BottomNavigation component following the material design mation guidelines
    
    * Edit based on feedback in pull request
    
    * Minor bug fix and added nativa animation driver support for android
    
    * Fixed lint error.
    
    * Added useNAtiveDriver and fixed the heigth issue
    
    * Fix getting height of bottom navigation
    
    * Added show and hide functionality
    
    * Fixed some lint errors
    
    * Fixed  bug where styles were wrongly refrenced

--- src/Toolbar/Toolbar.react.js
+++ src/Toolbar/Toolbar.react.js
@@ -10,6 +10,8 @@ import {
     TextInput,
     TouchableWithoutFeedback,
     View,
+    Platform,
+    Easing,
 } from 'react-native';
 /* eslint-enable import/no-unresolved, import/extensions */
 import IconToggle from '../IconToggle';
@@ -84,6 +86,10 @@ const propTypes = {
     */
     translucent: PropTypes.bool,
     /**
+    * Wether or not the Toolbar should show
+    */
+    hidden: PropTypes.bool,
+    /**
     * Called when centerElement was pressed.
     * TODO: better to rename to onCenterElementPress
     */
@@ -146,6 +152,7 @@ const propTypes = {
 const defaultProps = {
     elevation: 4, // TODO: probably useless, elevation is defined in getTheme function
     style: {},
+    hidden: false,
 };
 const contextTypes = {
     uiTheme: PropTypes.object.isRequired,
@@ -219,6 +226,7 @@ class Toolbar extends PureComponent {
         this.state = {
             isSearchActive: props.isSearchActive,
             searchValue: '',
+            moveAnimated: new Animated.Value(0),
         };
     }
     componentWillReceiveProps(nextProps) {
@@ -229,6 +237,14 @@ class Toolbar extends PureComponent {
         // searchable is set and isSearchActive is true, then we need to listen back button
         if (nextProps.searchable && this.state.isSearchActive) {
             this.backButtonListener = addBackButtonListener(this.onSearchCloseRequested);
+        }
+        // if hidden prop is changed we animate show or hide
+        if (nextProps.hidden !== this.props.hidden) {
+            if (nextProps.hidden === true) {
+                this.hide();
+            } else {
+                this.show();
+            }
         }
     }
     onMenuPressed = (labels) => {
@@ -268,7 +284,6 @@ class Toolbar extends PureComponent {
             searchable.onSearchPressed();
         }
 
-
         this.setState({
             isSearchActive: true,
             searchValue: '',
@@ -283,7 +298,6 @@ class Toolbar extends PureComponent {
             searchable.onSearchClosed();
         }
 
-
         this.setState({
             isSearchActive: false,
             searchValue: '',
@@ -291,6 +305,25 @@ class Toolbar extends PureComponent {
     };
     focusSearchField() {
         this.searchFieldRef.focus();
+    }
+    show = () => {
+        const { moveAnimated } = this.state;
+        Animated.timing(moveAnimated, {
+            toValue: 0,
+            duration: 225,
+            easing: Easing.bezier(0.0, 0.0, 0.2, 1),
+            useNativeDriver: Platform.OS === 'android',
+        }).start();
+    }
+    hide = () => {
+        const { moveAnimated } = this.state;
+        const styles = getStyles(this.props, this.context, this.state);
+        Animated.timing(moveAnimated, {
+            toValue: (-1 * StyleSheet.flatten(styles.container).height),
+            duration: 195,
+            easing: Easing.bezier(0.4, 0.0, 0.6, 1),
+            useNativeDriver: Platform.OS === 'android',
+        }).start();
     }
     renderLeftElement = (style) => {
         const { searchable, leftElement, onLeftElementPress, size } = this.props;
@@ -496,7 +529,13 @@ class Toolbar extends PureComponent {
         const styles = getStyles(this.props, this.context, this.state);
 
         return (
-            <Animated.View style={styles.container}>
+            <Animated.View
+                style={[styles.container, {
+                    transform: [{
+                        translateY: this.state.moveAnimated,
+                    }],
+                }]}
+            >
                 {this.renderLeftElement(styles)}
                 {this.renderCenterElement(styles)}
                 {this.renderRightElement(styles)}
