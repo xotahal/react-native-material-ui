acca11a383ce8e7e1daf6e17fcca83fa028535f2
Jiří Otáhal <xotahal@gmail.com>
2017-04-20 20:13:37 +0200

    [ActionButton] Remove scale animation from action button
    It cut the shadow of action button. We need to redesign whole action button
    component.

--- src/ActionButton/ActionButton.react.js
+++ src/ActionButton/ActionButton.react.js
@@ -7,8 +7,6 @@ import {
     StyleSheet,
     TouchableWithoutFeedback,
     Animated,
-    Easing,
-    Platform,
 } from 'react-native';
 /* eslint-enable import/no-unresolved, import/extensions */
 import Icon from '../Icon';
@@ -217,22 +215,22 @@ class ActionButton extends PureComponent {
         }
     }
     show = () => {
-        Animated.timing(this.state.scaleValue, {
-            toValue: 1,
-            duration: 225,
-            easing: Easing.bezier(0.0, 0.0, 0.2, 1),
-            useNativeDriver: Platform.OS === 'android',
-        }).start();
+        // Animated.timing(this.state.scaleValue, {
+        //     toValue: 1,
+        //     duration: 225,
+        //     easing: Easing.bezier(0.0, 0.0, 0.2, 1),
+        //     useNativeDriver: Platform.OS === 'android',
+        // }).start();
     }
     hide = () => {
-        Animated.timing(this.state.scaleValue, {
-            // TODO: why is not 0 here?
-            // see: https://github.com/facebook/react-native/issues/10510
-            toValue: 0.01,
-            duration: 195,
-            easing: Easing.bezier(0.4, 0.0, 0.6, 1),
-            useNativeDriver: Platform.OS === 'android',
-        }).start();
+        // Animated.timing(this.state.scaleValue, {
+        //     // TODO: why is not 0 here?
+        //     // see: https://github.com/facebook/react-native/issues/10510
+        //     toValue: 0.01,
+        //     duration: 195,
+        //     easing: Easing.bezier(0.4, 0.0, 0.6, 1),
+        //     useNativeDriver: Platform.OS === 'android',
+        // }).start();
     }
     renderToolbarTransition = (styles) => {
         const { actions } = this.props;
@@ -400,13 +398,7 @@ class ActionButton extends PureComponent {
         );
     }
     renderButton = styles => (
-        <Animated.View
-            style={[styles.positionContainer, {
-                transform: [{
-                    scale: this.state.scaleValue,
-                }],
-            }]}
-        >
+        <Animated.View style={styles.positionContainer}>
             {this.renderMainButton(styles)}
         </Animated.View>
     );
