bfdf17ff8f7b300cef6b00a6145d56cb0fcfb437
Jan Ziemba <jan.ziemba@gmail.com>
2018-06-08 10:24:31 +0200

    Update snackbar to follow new material guidelines, to support onPress and dynamic height

--- src/styles/spacing.js
+++ src/styles/spacing.js
@@ -3,5 +3,4 @@ export default {
     // https://material.google.com/layout/metrics-keylines.html#metrics-keylines-touch-target-size
     iconSize: 24,
     avatarSize: 40,
-    snackbarHeight: 48,
 };
