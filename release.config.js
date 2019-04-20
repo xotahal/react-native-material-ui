a8a932a4cb20578e26452cda0e0f4e80aed1c193
Jiri Otahal <xotahal@gmail.com>
2019-04-20 14:47:12 +1200

    chore: upgrade semantic-release

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  branches: [
+    { name: 'master', type: 'release' },
+    { name: 'develop', type: 'prerelease', prerelease: 'alpha' },
+    { name: 'beta', type: 'prerelease', prerelease: 'beta' },
+    { name: 'rc-*', type: 'prerelease', prerelease: 'rc' },
+  ],
+};
