511388652f2fe2ac6d31e8a480beb9fdcfcdd61a
PRODUCTION-NEW\vijay <vijays@sbiteam.com>
2023-04-18 13:11:37 +0530

    empty lines added

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
 import { View, BackHandler } from 'react-native';
+
 import { ViewPropTypes as RNViewPropTypes } from 'deprecated-react-native-prop-types';
+
 const ViewPropTypes = RNViewPropTypes || View.propTypes; // eslint-disable-line
 const BackAndroid = BackHandler; // BackAndroid is not supported anymore in react-native.
 
